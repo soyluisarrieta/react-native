2ecf3ff079a7233c3a4f6a75789af24b7db4d5a1
Luis Arrieta <luisarrieta796@gmail.com>
2023-08-07 20:25:05 -0500

    Create and stylize an app bar tab

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,26 +1,44 @@
 import Constants from 'expo-constants'
-import { StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import StyledText from './StyledText'
 import theme from '../theme.js'
+import { Link, useLocation } from 'react-router-native'
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.appBar.bgColor,
     paddingTop: Constants.statusBarHeight + 10,
-    paddingBottom: 10,
-    paddingHorizontal: 10
+    paddingBottom: 10
   },
-  text: {
-    color: theme.appBar.textColor
+  scroll: {
+    paddingBottom: 15
   }
 })
+
+function AppBarTab ({ children, to }) {
+  const { pathname } = useLocation()
+  const isActive = pathname === to
+  return (
+    <Link to={to}>
+      <StyledText
+        fontWeight='bold'
+        color={isActive ? 'primary' : 'muted'}
+        style={{ paddingHorizontal: 10 }}
+      >
+        {children}
+      </StyledText>
+
+    </Link>
+  )
+}
 
 function AppBar () {
   return (
     <View style={styles.container}>
-      <StyledText fontWeight='bold' style={styles.text}>
-        Repositories
-      </StyledText>
+      <ScrollView horizontal style={styles.scroll}>
+        <AppBarTab to='/'>Repositories</AppBarTab>
+        <AppBarTab to='/signin'>Sign In</AppBarTab>
+      </ScrollView>
     </View>
   )
 }

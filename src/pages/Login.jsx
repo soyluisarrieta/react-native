d9ca6200c681d4cb9a543e43dff8a3ab95d49af9
Luis Arrieta <luisarrieta796@gmail.com>
2023-08-07 21:25:52 -0500

    Validate fields in real time

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,9 +25,23 @@ function FormikInputValue ({ name, ...props }) {
   )
 }
 
+function validate (values) {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Email is required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  console.error({ errors })
+
+  return errors
+}
+
 function Login () {
   return (
     <Formik
+      validate={validate}
       initialValues={initialValues}
       onSubmit={values => console.log(values)}
     >

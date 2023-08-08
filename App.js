fc628d2fbd13d5fb69f6106d720444e4855685e8
Luis Arrieta <luisarrieta796@gmail.com>
2023-08-07 20:36:47 -0500

    Stylize bar top icons

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { NativeRouter } from 'react-router-native'
 import Main from './src/components/Main'
+import { StatusBar } from 'react-native'
 
 export default function App () {
-  return <NativeRouter><Main /></NativeRouter>
+  return (
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+    </>
+  )
 }

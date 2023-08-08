aca9b45966fbfb462ba3957308e64d22e16ea8b9
Luis Arrieta <luisarrieta796@gmail.com>
2023-08-07 21:04:03 -0500

    Add login form usign Formik

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,35 @@
-import { Text } from 'react-native'
+import { Formik } from 'formik'
+import { Button, TextInput, View } from 'react-native'
+
+const initialValues = {
+  email: '',
+  password: ''
+}
 
 function Login () {
   return (
-    <Text>
-      Login
-    </Text>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={values => console.log(values)}
+    >
+      {({ handleChange, handleSubmit, values }) => {
+        return (
+          <View>
+            <TextInput
+              placeholder='Email'
+              value={values.email}
+              onChangeText={handleChange('email')}
+            />
+            <TextInput
+              placeholder='Password'
+              value={values.password}
+              onChangeText={handleChange('password')}
+            />
+            <Button onPress={handleSubmit} title='Login' />
+          </View>
+        )
+      }}
+    </Formik>
   )
 }
 

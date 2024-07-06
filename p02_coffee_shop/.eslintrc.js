678d2800dd7f36f8f99e375fc74ab7176c03490d
Luis Arrieta <luisarrieta796@gmail.com>
2024-07-06 13:23:20 -0500

    Uninstall prettier, configure eslint rules and configure auto-fix when is on save file

--- p02_coffee_shop/.eslintrc.js
+++ p02_coffee_shop/.eslintrc.js
@@ -2,11 +2,56 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {
-    'prettier/prettier': [
+    semi: ['error', 'never'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    'no-unused-vars': [
       'error',
-      {
-        endOfLine: 'auto',
-      },
+      { vars: 'all', args: 'none', ignoreRestSiblings: false }
     ],
-  },
-};
+    'space-before-function-paren': ['error', 'always'],
+    'keyword-spacing': ['error', { before: true, after: true }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'comma-dangle': ['error', 'never'],
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    eqeqeq: ['error', 'always'],
+    'no-duplicate-imports': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'block-spacing': ['error', 'always'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    camelcase: ['error', { properties: 'always' }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'eol-last': ['error', 'always'],
+    'func-call-spacing': ['error', 'never'],
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'new-parens': 'error',
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-tabs': 'error',
+    'no-whitespace-before-property': 'error',
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': 'error',
+    'space-unary-ops': ['error', { words: true, nonwords: false }],
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        line: {
+          markers: ['/'],
+          exceptions: ['-', '+']
+        },
+        block: {
+          markers: ['!'],
+          exceptions: ['*'],
+          balanced: true
+        }
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': ['error'],
+
+    // Desactiva Prettier
+    'prettier/prettier': 'off'
+  }
+}

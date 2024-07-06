9653894edc65736c119dc2f5548a9553933761c0
Luis Arrieta <luisarrieta796@gmail.com>
2024-07-06 13:55:34 -0500

    Clean app.tsx and create screens files

---
+++ p02_coffee_shop/src/screens/DetailsScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function DetailsScreen () {
+  return (
+    <View>
+      <Text>DetailsScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ p02_coffee_shop/src/screens/PaymentScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function PaymentScreen () {
+  return (
+    <View>
+      <Text>PaymentScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
